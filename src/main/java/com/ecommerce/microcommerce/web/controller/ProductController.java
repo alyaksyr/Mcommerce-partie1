76ae6a7fe8ad8d7ea0fbbc0957f36ffcd531dc95
alyaksyr <alyaksyr3516@gmail.com>
2019-03-11 17:45:02 +0100

    Mise à jour de la methode ajouterProduit

--- src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
+++ src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.ecommerce.microcommerce.web.controller;
 
 import com.ecommerce.microcommerce.dao.ProductDao;
 import com.ecommerce.microcommerce.model.Product;
+import com.ecommerce.microcommerce.web.exceptions.ProduitGratuitException;
 import com.ecommerce.microcommerce.web.exceptions.ProduitIntrouvableException;
 import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
@@ -71,20 +72,24 @@ public class ProductController {
     //ajouter un produit
     @PostMapping(value = "/Produits")
 
-    public ResponseEntity<Void> ajouterProduit(@Valid @RequestBody Product product) {
+    public ResponseEntity<Void> ajouterProduit(@RequestBody Product product) {
 
-        Product productAdded =  productDao.save(product);
+        if (product.getPrix()== 0){
+            throw new ProduitGratuitException("Le prix de vente du produit ne doit pas être égal 0 !");
+        } else {
+            Product productAdded = productDao.save(product);
 
-        if (productAdded == null)
-            return ResponseEntity.noContent().build();
+            if (productAdded == null)
+                return ResponseEntity.noContent().build();
 
-        URI location = ServletUriComponentsBuilder
-                .fromCurrentRequest()
-                .path("/{id}")
-                .buildAndExpand(productAdded.getId())
-                .toUri();
+            URI location = ServletUriComponentsBuilder
+                    .fromCurrentRequest()
+                    .path("/{id}")
+                    .buildAndExpand(productAdded.getId())
+                    .toUri();
 
-        return ResponseEntity.created(location).build();
+            return ResponseEntity.created(location).build();
+        }
     }
 
     @DeleteMapping (value = "/Produits/{id}")
